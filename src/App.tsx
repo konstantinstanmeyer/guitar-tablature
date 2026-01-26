c7cb52ef2835ffac46c77c0c42873b82ef9e6b34
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-26 13:25:27 -0500

    type fixing for TabLine + createEmptyLine function def

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 
 import { useEffect, useMemo, useState } from 'react';
+import type { TabLine } from "../types/types"
 
 // Standard tuning, to later get handling for alternative tuning/more strings
 const STRING_NAMES = ['e', 'B', 'G', 'D', 'A', 'E'];
@@ -9,6 +10,18 @@ const STRING_NAMES = ['e', 'B', 'G', 'D', 'A', 'E'];
 // NOTE: Adjust post-testing for common musical phrase length
 // Set as a uniform value for visual conformity when exporting to pdf
 const DEFAULT_LENGTH = 40;
+
+function generateId(): string {
+  // getting base-36 string after "0." until the 9th index, nearly impossible to ever collide with previous values
+  return Math.random().toString(36).substring(2,9);
+}
+
+function createEmptyLine(length: number = DEFAULT_LENGTH): TabLine{
+  return {
+    id: generateId(),
+    strings: STRING_NAMES.map(() => '-'.repeat(length))
+  }
+}
 
 function App() {
   
--- types/types.ts
+++ types/types.ts
@@ -1,4 +1,4 @@
 export interface TabLine {
     id: string;
-    string: string[];
+    strings: string[];
 }

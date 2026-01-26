00759ee30d7b391bfa2870feaca3cd52979ed26f
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-26 13:19:10 -0500

    basic types and constants set

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
 
+import { useEffect, useMemo, useState } from 'react';
+
+// Standard tuning, to later get handling for alternative tuning/more strings
+const STRING_NAMES = ['e', 'B', 'G', 'D', 'A', 'E'];
+
+// Default number of "columns", a.k.a. beats (tab equivalent), for each line of tablature
+// NOTE: Adjust post-testing for common musical phrase length
+// Set as a uniform value for visual conformity when exporting to pdf
+const DEFAULT_LENGTH = 40;
+
 function App() {
+  
+
   return (
-    <main className="bg-black">
-      <p className="text-red-500">hello</p>
+    <main className="">
+      
     </main>
   )
 }
---
+++ types/types.ts
@@ -0,0 +1,4 @@
+export interface TabLine {
+    id: string;
+    string: string[];
+}
